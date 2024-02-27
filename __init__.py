ba8146671afe48ad8bfe19d68658a1310acac95a
Rdtns <146824397+Rdtns@users.noreply.github.com>
2024-02-27 10:18:46 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask import json
 from urllib.request import urlopen
 import sqlite3
 
-app = Flask("name") #creating flask app name
+app = Flask(__name__) #creating flask app name
 #Création d'une nouvelle route pour la lecture de la BDD
 @app.route("/consultation/")
 def ReadBDD():
