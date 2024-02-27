54ef51ccf1e1864a88454d8b7f53a74dbdbeead0
Rdtns <146824397+Rdtns@users.noreply.github.com>
2024-02-27 11:15:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,60 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-# Autres routes...
+@app.route('/resume_1')
+def resume_1():
+    return render_template("resume_1.html")
+
+@app.route('/resume_2')
+def resume_2():
+    return render_template("resume_2.html")
+
+@app.route('/resume_template')
+def resume_template():
+    return render_template("resume_template.html")
+
+@app.route("/consultation/")
+def ReadBDD():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM utilisateurs;')
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
+
+@app.route('/fiche_client/<int:post_id>')
+def Readfiche(post_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM livres WHERE id = ?', (post_id,))
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
+
+@app.route('/fiche_clientn/<string:nom>')
+def Readfichenom(nom):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM livres WHERE nom LIKE ?', (nom,))
+    data = cursor.fetchall()
+    conn.close()
+    return render_template('read_data.html', data=data)
+
+@app.route('/search_client', methods=['GET', 'POST'])
+def Searchfiche():
+    if request.method == 'POST':
+        nom = request.form['nom']
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM livres WHERE nom = ?', (nom,))
+        data = cursor.fetchall()
+        conn.close()
+        if data:
+            return render_template('read_data.html', data=data)
+        else:
+            return "No client found with that name."
+    else:     
+       return "Method not allowed for..."
 
 @app.route('/ajouter_utilisateur/', methods=['GET', 'POST'])
 def ajouter_utilisateur():
@@ -20,20 +73,20 @@ def ajouter_utilisateur():
         nom = request.form['nom']
         prenom = request.form['prenom']
         message = request.form['message']
-
         conn = get_db_connection()
         cursor = conn.cursor()
-        try:
-            cursor.execute('INSERT INTO utilisateurs (nom, prenom, message) VALUES (?, ?, ?)', 
-                           (nom, prenom, message))
-            conn.commit()
-        except sqlite3.Error as e:
-            print(f"Erreur de base de données: {e}")
-            # Retourner une réponse ou rediriger avec un message d'erreur
-        finally:
-            conn.close()
+        if conn is not None:
+            try:
+                cursor.execute('INSERT INTO utilisateurs (nom, prenom, message) VALUES (?, ?, ?)', 
+                               (nom, prenom, message))
+                conn.commit()
+            except sqlite3.Error as e:
+                print(f"Erreur de base de données: {e}")
+                # Retourner une réponse ou rediriger avec un message d'erreur
+            finally:
+                conn.close()
 
-        return redirect('/consultation/')
+        return redirect('https://moulin2.alwaysdata.net/consultation/')
 
     return render_template('create_user.html')
 
@@ -42,7 +95,7 @@ def authentification():
     if request.method == 'POST':
         if request.form['username'] == 'admin' and request.form['password'] == 'password':
             session['authentifie'] = True
-            return redirect('/')
+            return redirect(url_for('lecture'))
         else:
             return render_template('formulaire_authentification.html', error=True)
 
