1ca6b74465adae99d0c039ceff1aadddbf0dfc0c
Rdtns <146824397+Rdtns@users.noreply.github.com>
2024-02-27 10:56:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,7 +9,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 @app.route('/')
 def home():
-    return render_template("resume_2.html")
+    return render_template("index.html")
+
+@app.route("/consultation/")
+def ReadBDD():
+    data = AddMessageModel.query.all()
+    return render_template('read_data.html', data=data)
 
 @app.route('/resume_1')
 def resume_1():
@@ -23,10 +28,7 @@ def resume_2():
 def resume_template():
     return render_template("resume_template.html")
 
-# Création d'une nouvelle route pour la lecture de la BDD
-@app.route("/consultation/")
-def ReadBDD():
-    data = AddMessageModel.query.all()
-    return render_template('read_data.html', data=data)
+if(__name__ == "__main__"):
+    app.run()
 
 
