21f7509397849a31f6b8b21c0ff6135173fddba2
Rdtns <146824397+Rdtns@users.noreply.github.com>
2024-03-05 11:35:35 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS Clients;
 CREATE TABLE Clients (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    father_name TEXT NOT NULL,
-    mother_name TEXT NOT NULL,
-    permanent_address NOT NULL    
+    fathername TEXT NOT NULL,
+    mothername TEXT NOT NULL,
+    permanentaddress NOT NULL    
 );
