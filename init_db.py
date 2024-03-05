d05fc4e4120b1709e25f22460c9e709adf781288
Rdtns <146824397+Rdtns@users.noreply.github.com>
2024-03-05 10:59:24 +0100

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -7,20 +7,20 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO Clients (title, auteur) VALUES (?, ?)",
-            ('Moby-Dick', 'Herman Melville')
+cur.execute("INSERT INTO Clients (father_name, mother_name, permanent_address) VALUES (?, ?, ?)",
+            ('Moby-Dick', 'Herman Melville', '137 BDV AUGUSTE')
             )
 
-cur.execute("INSERT INTO Clients (title, auteur) VALUES (?, ?)",
-            ('Notre-Dame de Paris', 'Victor Hugo')
+cur.execute("INSERT INTO Clients (father_name, mother_name, permanent_address) VALUES (?, ?, ?)",
+            ('Mick Hol', 'THANG', '1 BDV EMILE')
             )
 
-cur.execute("INSERT INTO Clients (title, auteur) VALUES (?, ?)",
-            ('CosmoZ', 'Christophe Claro')
+cur.execute("INSERT INTO Clients (father_name, mother_name, permanent_address) VALUES (?, ?, ?)",
+            ('BOBY POT', 'XUEE', '18 CHEMIN ROUGE')
             )
 
-cur.execute("INSERT INTO Clients (title, auteur) VALUES (?, ?)",
-            ('La reine du crime', 'Agatha Christie')
+cur.execute("INSERT INTO Clients (father_name, mother_name, permanent_address) VALUES (?, ?, ?)",
+            ('ZHANG THEODORE', 'LAURA RO', '118 RUE JOSEPH')
             )
 
 
